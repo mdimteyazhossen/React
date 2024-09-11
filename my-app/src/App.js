47ca71518aff29e408d6f3f7a029f76bd7827349
mdimteyazhossen <imteyazhossen13711@gmail.com>
2024-09-11 10:04:23 +0600

    class-11:Nested mapping

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,42 @@
 import React from "react";
-import List from "./components/UniqueList/list";
+const users=[
+    {
+        fullName:"Anis",
+        age:32,
+        phones:[
+            {home:"018454545"},
+            {office:"96545521"}
+        ]
+    },
+    {
+        fullName:"mahir",
+        age:22,
+        phones:[
+            {home:"15548796"},
+            {office:"95548123"}
+        ]
+    }
+]
 
 export default function App(){
-
-    // let items = [];
-    // items.push(<Card todotitle={Data[x].title} tododesc={Data[x].desc}/>)
-    // items = Data.map((item) => <Card todotitle={item.title} tododesc={item.desc}/>);
     return (
-        <List />
+        <div>
+            <h1>Nested List</h1>
+            {users.map((user, index) => (
+                // const {fullName,age,phone}=users;
+                <article key={index}>
+                    <h3>{user.fullName}</h3>
+                    <p>{user.age}</p>
+                    {
+                        user.phones.map((phone,index) => (
+                        <div key={index}>
+                            <p>Home: {phone.home}</p>
+                            <p>office: {phone.office}</p>
+                        </div>
+                   ))}
+                </article>
+            ))}
+        </div>
     )
 
 }
